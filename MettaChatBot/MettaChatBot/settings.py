208b22853382029d162e64453cc52baca15b5ec0
Abreham Gebremedhin <abreham.gmedhin12@gmail.com>
2024-07-19 20:06:30 +0300

    started the django implementation for the chatbot

--- MettaChatBot/MettaChatBot/settings.py
+++ MettaChatBot/MettaChatBot/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'MettaChatBot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MettaChatBot',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
