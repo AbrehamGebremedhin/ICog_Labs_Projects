1926bbf658a7c06819c3d2617d3d65bc18d1d853
Abreham Gebremedhin <abreham.gmedhin12@gmail.com>
2024-07-22 11:25:53 +0300

    refactored the scraper to output in csv format as well

--- MettaChatBot/Scraper.py
+++ MettaChatBot/Scraper.py
@@ -1,4 +1,5 @@
 import time
+import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -32,6 +33,7 @@ def clean_text(text):
 
 class Scraper:
     def __init__(self, url) -> None:
+        self.output_type = "structured"
         self.url_stack = [url]
         self.base_url = url
         self.base_domain = urlparse(url).netloc
@@ -40,6 +42,7 @@ class Scraper:
         self.soup = None
 
     def find_all(self):
+        data = list()
         # Iterate through each URL in the stack
         for url in self.url_stack:
             # Skip the URL if it has already been visited
@@ -63,59 +66,80 @@ class Scraper:
 
                 # Process the link if it's not already visited
                 if href.startswith('http') and href not in self.visited_urls:
-                    # Open the output file in append mode
-                    with open('output.txt', 'a', encoding='utf-8') as file:
-                        try:
-                            # Fetch the content of the link
-                            response = driver(href)
-                            soup = BeautifulSoup(response, 'html.parser')
-                            # Try to find the main content in <main> or <article> tags
-                            soup = soup.find('main') or soup.find('article', soup)
 
-                            # Process each <li> tag in the document
-                            for li in soup.find_all('li'):
-                                # Extract and concatenate text from all <code> tags within the <li>
-                                code_texts = ' '.join(
-                                    tag.get_text(separator=' ', strip=True) for tag in li.find_all('code'))
-                                # Replace <code> tags with their concatenated text
-                                for tag in li.find_all('code'):
-                                    tag.replace_with(code_texts)
-                                # Update the <li> tag with modified text
-                                li_text = li.get_text(separator=' ', strip=True)
-                                li.string = li_text
+                    try:
+                        # Fetch the content of the link
+                        response = driver(href)
+                        soup = BeautifulSoup(response, 'html.parser')
+                        ul_tags = soup.find_all('ul', class_="VPDocOutlineItem root")
+                        # Try to find the main content in <main> or <article> tags
+                        soup = soup.find('main') or soup.find('article', soup)
 
-                            # Process each <pre> tag in the document
-                            for pre in soup.find_all('pre'):
-                                # Extract and concatenate text from all <code> tags within the <pre>
-                                code_texts = []
-                                for code in pre.find_all('code'):
-                                    # Extract text from all <span> tags within the <code>
-                                    span_texts = ' '.join(
-                                        span.get_text(separator=' ', strip=True) for span in code.find_all('span'))
-                                    code_texts.append(span_texts)
-                                # Concatenate all code texts into a single line
-                                concatenated_code_texts = ' '.join(code_texts)
-                                # Remove <code> tags from the <pre> content
-                                for code in pre.find_all('code'):
-                                    code.replace_with('')
-                                # Update the <pre> tag with modified content
-                                pre_text = pre.get_text(separator=' ', strip=True)
-                                pre.string = f"{pre_text} {concatenated_code_texts}"
+                        # Process each <li> tag in the document
+                        for li in soup.find_all('li'):
+                            # Extract and concatenate text from all <code> tags within the <li>
+                            code_texts = ' '.join(
+                                tag.get_text(separator=' ', strip=True) for tag in li.find_all('code'))
+                            # Replace <code> tags with their concatenated text
+                            for tag in li.find_all('code'):
+                                tag.replace_with(code_texts)
+                            # Update the <li> tag with modified text
+                            li_text = li.get_text(separator=' ', strip=True)
+                            li.string = li_text
 
-                            # Get the modified text of the whole document
-                            text = soup.get_text(separator='\n', strip=True)
-                            # Write the modified text to the output file
-                            file.write(f"{text}\n")
+                        # Process each <pre> tag in the document
+                        for pre in soup.find_all('pre'):
+                            # Extract and concatenate text from all <code> tags within the <pre>
+                            code_texts = []
+                            for code in pre.find_all('code'):
+                                # Extract text from all <span> tags within the <code>
+                                span_texts = ' '.join(
+                                    span.get_text(separator=' ', strip=True) for span in code.find_all('span'))
+                                code_texts.append(span_texts)
+                            # Concatenate all code texts into a single line
+                            concatenated_code_texts = ' '.join(code_texts)
+                            # Remove <code> tags from the <pre> content
+                            for code in pre.find_all('code'):
+                                code.replace_with('')
+                            # Update the <pre> tag with modified content
+                            pre_text = pre.get_text(separator=' ', strip=True)
+                            pre.string = f"{pre_text} {concatenated_code_texts}"
 
-                        except Exception as e:
-                            # Log any exceptions encountered during processing
-                            print(f"Failed to scrape {href}: {e}")
+                        # Get the modified text of the whole document
+                        text = soup.get_text(separator='\n', strip=True)
+                        keywords = list()
+                        for ul in ul_tags:
+                            # Find all <li> tags within the current <ul> tag
+                            li_tags = ul.find_all('li')
+
+                            # Loop through and print each <li> tag
+                            for li in li_tags:
+                                keywords.append(li.text)
+                        data.append({"URL": href, "Text": text, "Keywords": keywords})
+
+                    except Exception as e:
+                        # Log any exceptions encountered during processing
+                        print(f"Failed to scrape {href}: {e}")
                     # Add the processed URL to the list of visited URLs
                     self.visited_urls.append(href)
 
             # Pause execution for 10 seconds to avoid overwhelming the server
             time.sleep(10)
+        return data
+
+    def structured(self):
+        data = self.find_all()
+        if self.output_type != "structured":
+            with 'output.txt', 'a' as f:
+                for item in data:
+                    f.write(f"{item['Text']}\n")
+                    f.write("\n")
+            return data
+        else:
+            df = pd.DataFrame(data)
+            df.to_csv("data.csv", index=False)
+            return df
 
 
 scrape = Scraper("https://metta-lang.dev/docs/learn/learn.html")
-scrape.find_all()
+print(scrape.structured())
